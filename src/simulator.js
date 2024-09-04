e0f0fd3b2f273a5dbad3ba944c87b3181e77e240
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-09-04 13:52:35 -0400

    Schematic of the car simulation
    Second implementation of the car simulator

---
+++ src/simulator.js
@@ -0,0 +1,14 @@
+export default class AutoSimulator {
+    constructor(max_x, max_y, initial_x, initial_y, initial_orientation) {
+      this.max_x = max_x;
+      this.max_y = max_y;
+      this.x = initial_x;
+      this.y = initial_y;
+      this.orientation = initial_orientation;
+    }
+    execute_commands(commands) {
+      const initial_position = `${this.x},${this.y}${this.orientation}`;
+      const final_position = `${this.x},${this.y}${this.orientation}`;
+      return { initial_position, final_position };
+    }
+}
